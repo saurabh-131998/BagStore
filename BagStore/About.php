cdce187e7a56dc60ce406cc38fb1476954748c20
saurabh-131998 <94494608+saurabh-131998@users.noreply.github.com>
2021-11-16 23:38:15 +0000

    Committing website proj

---
+++ BagStore/About.php
@@ -0,0 +1,99 @@
+<?php
+
+include("auth.php"); //include auth.php file on all secure pages ?>
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>About Us</title>
+
+    
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+   
+    <link href="css/shop-homepage.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+
+  
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+      <div class="container">
+        <a class="navbar-brand" href="#">Bag Store</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+              <a class="nav-link" href="index.php">Home
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#contact">Contact</a>
+            </li>
+			<li class="nav-item">
+              <a class="nav-link" href="logout.php">Logout</a>
+            </li>
+          </ul>
+        </div>
+		
+      </div>
+    </nav>
+	</br>
+			<p align="center"><B>WELCOME <?php echo $_SESSION['username']; ?>!</B></p>
+	
+	
+	
+	<div class="container">
+
+      <div class="row">
+
+        <div class="col-lg-3">
+
+          <h1 class="my-4">OneStop</h1>
+        </div>
+		
+		
+	   
+              </div>
+            </div>
+
+          </div>
+         
+
+        </div>
+     
+
+      </div>
+      
+
+    </div>
+	<p><center><B></br></br></br></br></br></br></br>Let’s face it, everybody loves a great bag! The only frustrating thing about a great bag is finding one! Yet, what actually constitutes a great bag? Is it the fact that it carries all your essentials? That it can be matched with every outfit in your closet? That both form and function standards are met? we at OneStop Help and Provide our customers with best of the commodities so that they shine in the crowd! Along with our contacts of distributers and Suppliers we will thrive for global market, Providing our bags to people worldwide, Because You My Friend are Special So does Should Your Bag Be! </B></center></p>
+   
+	 </br></br></br></br></br></br></br> 
+	  <footer class="py-5 bg-dark">
+      <div class="container" id="contact">
+        <p class="m-0 text-center text-white">Copyright &copy; Bag Store</br>Contact: Saurabh.Shukla0000@yahoo.com-9022009981</p>
+      </div>
+     
+    </footer>
+
+   
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  </body>
+
+</html>
